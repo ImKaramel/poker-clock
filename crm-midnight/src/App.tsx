c08e50234afa3f5267826a3dd6357b73d9e1029a
klu4 <test@MB426657.local>
2026-04-10 20:50:23 +0300

    changed initdata

--- crm-midnight/src/App.tsx
+++ crm-midnight/src/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => {
             );
 
           const response = await authAPI.telegramInitAuth(initDataUnsafe);
-          // console.log(initData)
+          console.log(initDataUnsafe)
 
           if (!response.data?.token) {
             throw new Error("No token in API response");
