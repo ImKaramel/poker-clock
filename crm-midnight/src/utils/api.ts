fd3ed518377dc0b642988a101815ab4bf960eb1e
klu4 <test@MB426657.local>
2026-04-11 13:58:05 +0300

    fix data api

--- crm-midnight/src/utils/api.ts
+++ crm-midnight/src/utils/api.ts
@@ -55,7 +55,7 @@ export const ratingAPI = {
 
 export const profileAPI = {
   getProfile: () => api.get('/profile'),
-  updateProfile: (data: any) => api.patch('/profile/', data),
+  updateProfile: (data: any) => api.patch('/profile/', { data }),
 };
 
 export const supportAPI = {
