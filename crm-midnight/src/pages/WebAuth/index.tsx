b33e62d3aa5529d2dd1488a071068c766efb7b13
klu4 <test@MB426657.local>
2026-04-20 23:40:53 +0300

    fix route

--- crm-midnight/src/pages/WebAuth/index.tsx
+++ crm-midnight/src/pages/WebAuth/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import background from "../../assets/background.jpg";
 
 const WebAuth: React.FC = () => {
